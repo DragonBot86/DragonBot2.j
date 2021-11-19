d49a3a534f41e57d53cce34efaf7e06791700b98
DragonBot86 <93496266+DragonBot86@users.noreply.github.com>
2021-11-18 21:50:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -255,7 +255,14 @@ async function starts() {
 			console.log(anu)
 			if (anu.action == 'add') {
 				num = anu.participants[0]
-				teks = `¡HOLA NEFASTO! @${num.split('@')[0]} Un gusto conocerte🐉\n\n『Bienvenido/a a ${mdata.subject}』\n\n《⛓️》Presentarse o ban《⛓️》\n\n★➪Evita hacer pendejadas y cumple las reglas, ok?\n\n★➪𝗡𝗼 𝘃𝗲𝗻𝗴𝗮𝘀 𝗵𝗮𝗰𝗲𝗿 𝗯𝗮𝗿𝗱𝗼, 𝗽𝗲𝗻𝗱𝗲𝗷𝗼, 𝗲𝗱𝗮𝗱 𝗽𝗲𝗿𝗺𝗶𝘁𝗶𝗱𝗮 𝟷𝟺 𝛂 𝟸𝟻`
+				teks = `¡HOLA NEFASTO! @${num.split('@')[0]} Un gusto conocerte🐉\n\n『Bienvenido/a a ${mdata.subject}』\n\n《⛓️》Presentarse o ban《⛓️》\n\n★➪Evita hacer pendejadas y cumple las reglas, ok?\n\n┈┈┏━╮╭━┓┈╭━━━━━━╮
+┈┈┃┏┗┛┓┃╭┫ⓞⓘⓝⓚ┃
+┈┈╰┓▋▋┏╯╯╰━━━━━━╯
+┈╭━┻╮╲┗━━━━╮╭╮┈
+┈┃▎▎┃╲╲╲╲╲╲┣━╯┈
+┈╰━┳┻▅╯╲╲╲╲┃┈┈┈
+┈┈┈╰━┳┓┏┳┓┏╯┈┈┈
+┈┈┈┈┈┗┻┛┗┻┛┈┈┈┈`
                           client.sendMessage(mdata.id, teks, MessageType.text, { contextInfo: {"mentionedJid": [num]}})
 			} else if (anu.action == 'remove') {
 				num = anu.participants[0]
